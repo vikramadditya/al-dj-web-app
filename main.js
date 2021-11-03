1915d3e7837b0dc390924ccc6d9519430a36bf5d
vikramadditya <86791011+vikramadditya@users.noreply.github.com>
2021-11-03 22:26:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+song = "";
+
+function preload()
+{
+	song = loadSound("music.mp3");
+}
+
+scoreRightWrist = 0;
+scoreLeftWrist = 0;
+
+rightWristX = 0;
+rightWristY = 0;
+
+leftWristX = 0;
+leftWristY = 0;
+
+function setup() {
+	canvas =  createCanvas(600, 500);
+	canvas.center();
+
+	video = createCapture(VIDEO);
+	video.hide();
+
+	poseNet = ml5.poseNet(video, modelLoaded);
+	poseNet.on('pose', gotPoses);
+}
